0698413e58a065454829e0665daaeac71ba95a62
Matt Trussell <smtrussell@gmail.com>
2018-10-17 06:12:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function printBadges(arr){
+  for(let i = 0; i < arr.length; i++){
+    let employeeNumber = i + 1;
+    console.log(`Welcome ${arr[i]}! You are employee #${employeeNumber}.`);
+  }
+  return arr;
+}
+
+
+function tailsNeverFails(){
+  let flip = 0.5;
+  let count = 0;
+  while(flip >= 0.5){
+    flip = Math.random();
+    if (flip >= 0.5){
+      count += 1;
+    }
+  }
+  return `You got ${count} tails in a row!`;
+}
