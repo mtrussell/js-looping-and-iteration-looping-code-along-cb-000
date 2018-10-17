b2b33cf1dca804d1732bfde8ef95157586bdb3e5
Matt Trussell <smtrussell@gmail.com>
2018-10-17 05:58:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function printBadges(arr){
   for(let i = 0; i < arr.length; i++){
     let employeeNumber = i + 1;
-    console.log("Welcome ${arr[i]}! You are employee #  ${employeeNumber} .");
+    console.log(`Welcome ${arr[i]}! You are employee #  ${employeeNumber} .`);
   }
 }
 
